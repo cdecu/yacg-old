bed3ac41cad277112bb4e46e81f45d9eadb91bef
Carlos de Cumont <carlos@decumont.be>
2021-05-26 17:46:13 +0200

    Test github actions

--- apps/ng-yacg/src/polyfills.ts
+++ apps/ng-yacg/src/polyfills.ts
@@ -63,3 +63,10 @@
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'codemirror/mode/pascal/pascal';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/lib/codemirror';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchbrackets';
